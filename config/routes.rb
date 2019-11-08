d5fc393868b68e7d762f2e05413b015140443426
Thao.Hoang <thaominhhoangvn@outlook.com>
2019-11-08 22:12:02 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
-  get '/signup', to: 'users#new'
   resources :users
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 end
