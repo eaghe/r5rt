09c5a64e9d597c5cfb4c8acd7d6dbe17adc66469
Thao Hoang <thaominhhoagnvn@outlook.com>
2019-11-08 11:41:07 +0700

    full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    if page_title.empty?
+      base_title
+    else
+      page_title << " | " << base_title
+    end
+  end
+
   def base_title
     "Ruby on Rails Tutorial Sample App"
   end
