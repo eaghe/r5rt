a731fb91fcb3f3bfb4877a99787994dcf845ba82
Thao.Hoang <thaominhhoangvn@outlook.com>
2019-11-09 15:37:43 +0700

    follow and unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
@@ -15,4 +19,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
